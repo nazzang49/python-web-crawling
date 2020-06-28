bd63dbdea6f2f2d70ed8327b37dce2c7f7d12c41
jypark03 <jypark03@simplexi.com>
2020-06-28 19:11:45 +0900

    crawling_test

---
+++ test/crawling_google_img_1.py
@@ -0,0 +1,47 @@
+# crawling of google img
+# keyword is 카페
+from bs4 import BeautifulSoup
+from urllib.parse import quote_plus
+from urllib.request import urlretrieve, urlopen
+from selenium import webdriver
+import sys
+import io
+
+# for printing korean
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding="utf-8")
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding="utf-8")
+
+# input keyword
+search_keyword = input("검색어를 입력하세요 : ")
+# string formatting
+url = f"https://www.google.com/search?q={quote_plus(search_keyword)}&sxsrf=ALeKk01n9CbhJFSTR5dLoacGQlXGMfTBTg:1593337667405&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjY0rCtnaTqAhWUQN4KHcV3B5oQ_AUoAnoECA4QBA&biw=1920&bih=987"
+
+driver = webdriver.Chrome("D:/chromedriver.exe")
+# waiting for 3sec
+driver.implicitly_wait(3)
+driver.get(url)
+
+for _ in range(10000):
+    driver.execute_script("window.scrollBy(0,30000)")
+
+count = 0
+photo_list = []
+
+photo_list = driver.find_elements_by_tag_name("img.rg_i.Q4LuWd")
+
+for index, img in enumerate(photo_list[0:]):
+    # click default img
+    webdriver.ActionChains(driver).move_to_element(img).click(img).perform()
+    html_objects = driver.find_element_by_tag_name('img.n3VNCb')
+    current_src = html_objects.get_attribute('src')
+
+    t = urlopen(current_src).read()
+    if index < 600:
+        filename = search_keyword + str(count) + ".jpg"
+        File = open('D:/cafe-crawling-chrome/' + search_keyword + str(count) + '.jpg', 'wb')
+        File.write(t)
+        count += 1
+        print("img-save" + str(count))
+    else:
+        driver.close()
+        break
--- test/crawling_naver_img.py
+++ test/crawling_naver_img.py
@@ -2,7 +2,7 @@
 # keyword is 카페
 from bs4 import BeautifulSoup
 from urllib.parse import quote_plus
-from urllib.request import urlretrieve
+from urllib.request import urlretrieve, urlopen
 from selenium import webdriver
 import sys
 import io
@@ -14,7 +14,7 @@ sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding="utf-8")
 # input keyword
 search_keyword = input("검색어를 입력하세요 : ")
 # string formatting
-url = f"https://www.google.com/search?q={quote_plus(search_keyword)}&sxsrf=ALeKk01Kf9wYcHp9ra4r0mAha2cwUxXj3w:1593326226702&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi7zYPe8qPqAhXWdXAKHU9-BNwQ_AUoAnoECA4QBA&biw=1920&bih=1036"
+url = f"https://search.naver.com/search.naver?where=image&sm=tab_jum&query={quote_plus(search_keyword)}"
 
 driver = webdriver.Chrome("D:/chromedriver.exe")
 driver.get(url)
@@ -24,27 +24,20 @@ bs = BeautifulSoup(html, "html.parser")
 # scroll reaction
 # max 400 collect
 for i in range(500):
-    driver.execute_script("window.scrollBy(0, 10000)")
+    driver.execute_script("window.scrollBy(0, 500)")
 
 # find tags which class name is .rg_i.Q4LuWd
-img = bs.select(".rg_i.Q4LuWd")
-
-imgurl = []
-
-# only crawling src attributes
-for i in img:
-    try:
-        imgurl.append(i.attrs["src"])
-    except KeyError:
-        imgurl.append(i.attrs["data-src"])
+img = bs.find_all(class_="_img")
 
 n = 1
-# save as other name
-for i in imgurl:
-    urlretrieve(i, "D:/cafe-crawling/" + search_keyword + str(n) + ".jpg")
+for i in img:
+    imgUrl = i['data-source']
+    with urlopen(imgUrl) as f:
+        with open('D:/cafe-crawling-naver/' + search_keyword + str(n) + '.jpg', 'wb') as h:
+            img = f.read()
+            h.write(img)
     n += 1
-    # if n is 600:
-    #     break
+    print(imgUrl)
 
 # termination
 driver.close()
