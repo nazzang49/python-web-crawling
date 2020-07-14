72fa49963232a8a7a5e26256e312c4d3aa096306
박진영 <nazzang49@naver.com>
2020-07-15 01:21:45 +0900

    브라우저별 카페 이미지 크롤링 by batch

--- test/crawling_google_img_1.py
+++ test/crawling_google_img_1.py
@@ -6,17 +6,18 @@ from urllib.request import urlretrieve, urlopen
 from selenium import webdriver
 import sys
 import io
+import os
 
 # for printing korean
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding="utf-8")
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding="utf-8")
 
 # input keyword
-search_keyword = input("검색어를 입력하세요 : ")
+search_keyword = "카페"
 # string formatting
 url = f"https://www.google.com/search?q={quote_plus(search_keyword)}&sxsrf=ALeKk01n9CbhJFSTR5dLoacGQlXGMfTBTg:1593337667405&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjY0rCtnaTqAhWUQN4KHcV3B5oQ_AUoAnoECA4QBA&biw=1920&bih=987"
 
-driver = webdriver.Chrome("D:/chromedriver.exe")
+driver = webdriver.Chrome("C:/chromedriver_win32/chromedriver.exe")
 # waiting for 3sec
 driver.implicitly_wait(3)
 driver.get(url)
@@ -24,10 +25,15 @@ driver.get(url)
 for _ in range(10000):
     driver.execute_script("window.scrollBy(0,30000)")
 
-count = 0
 photo_list = []
 
 photo_list = driver.find_elements_by_tag_name("img.rg_i.Q4LuWd")
+
+# check current download img amounts -> preventing from overwritten
+current_img_count = len(os.walk("C:/cafe-crawling-chrome/").__next__()[2])
+print(current_img_count)
+
+count = current_img_count + 1
 
 for index, img in enumerate(photo_list[0:]):
     # click default img
@@ -38,7 +44,7 @@ for index, img in enumerate(photo_list[0:]):
     t = urlopen(current_src).read()
     if index < 600:
         filename = search_keyword + str(count) + ".jpg"
-        File = open('D:/cafe-cnn-deep-learning-crawling-chrome/' + search_keyword + str(count) + '.jpg', 'wb')
+        File = open('C:/cafe-crawling-chrome/' + search_keyword + str(count) + '.jpg', 'wb')
         File.write(t)
         count += 1
         print("img-save" + str(count))
--- test/crawling_naver_img_1.py
+++ test/crawling_naver_img_1.py
@@ -6,17 +6,18 @@ from urllib.request import urlretrieve, urlopen
 from selenium import webdriver
 import sys
 import io
+import os
 
 # for printing korean
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding="utf-8")
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding="utf-8")
 
 # input keyword
-search_keyword = input("검색어를 입력하세요 : ")
+search_keyword = "카페"
 # string formatting
 url = f"https://search.naver.com/search.naver?where=image&sm=tab_jum&query={quote_plus(search_keyword)}"
 
-driver = webdriver.Chrome("D:/chromedriver.exe")
+driver = webdriver.Chrome("C:/chromedriver_win32/chromedriver.exe")
 # waiting for 3sec
 driver.implicitly_wait(3)
 driver.get(url)
@@ -24,10 +25,14 @@ driver.get(url)
 for _ in range(10000):
     driver.execute_script("window.scrollBy(0,30000)")
 
-count = 0
 photo_list = []
 
 photo_list = driver.find_elements_by_tag_name("span.img_border")
+
+current_img_count = len(os.walk("C:/cafe-crawling-naver/").__next__()[2])
+print(current_img_count)
+
+count = current_img_count + 1
 
 for index, img in enumerate(photo_list[0:]):
     # click default img
@@ -38,7 +43,7 @@ for index, img in enumerate(photo_list[0:]):
     t = urlopen(current_src).read()
     if index < 700:
         filename = search_keyword + str(count) + ".jpg"
-        File = open('D:/cafe-cnn-deep-learning-crawling-naver/' + search_keyword + str(count) + '.jpg', 'wb')
+        File = open('C:/cafe-crawling-naver/' + search_keyword + str(count) + '.jpg', 'wb')
         File.write(t)
         count += 1
         print("img-save" + str(count))
