1f8ed532278764fa5a15ef7f86f448c20aafd3b9
박진영 <nazzang49@naver.com>
2020-08-04 01:51:10 +0900

    기존 이미지 파일명 prefix to additional imgs

--- deep_learning_model/generate_image_test.py
+++ deep_learning_model/generate_image_test.py
@@ -1,3 +1,5 @@
+# image generating test
+
 import glob
 from keras_preprocessing.image import ImageDataGenerator, load_img, img_to_array
 
@@ -21,13 +23,14 @@ for i, f in enumerate(img_files):
     x = x.reshape((1,) + x.shape)
 
     n = 1
+    img_file_name = f.split("\\")[1].split(".")[0]
     # control eternal loop by break
     for batch in data_aug_gen.flow(x,
                                    batch_size=1,
                                    save_to_dir='C:/cafe-img/ethnic',
-                                   save_prefix='ex_' + str(i),
+                                   save_prefix=img_file_name,
                                    save_format='jpg'):
         n += 1
         # generate 5 more images for every images
-        if n > 6:
+        if n > 5:
             break
